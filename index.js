5adfaea8af758c92fd079d0f69adc932b28cb71d
Rivelle <rivelle.days@gmail.com>
2025-06-10 17:16:40 +0800

    Update to Version 2.8.5
    Fixed an issue where character selection couldn’t be scrolled on mobile devices.
    Special thanks to qi_po for pointing out the code issue.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const EXTENSION_NAME = 'Moonlit Echoes Theme';
 const settingsKey = 'SillyTavernMoonlitEchoesTheme';
 const extensionName = "SillyTavern-MoonlitEchoesTheme";
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
-const THEME_VERSION = "2.8.4";
+const THEME_VERSION = "2.8.5";
 
 // Import required functions for drag functionality
 import { dragElement } from '../../../RossAscends-mods.js';
